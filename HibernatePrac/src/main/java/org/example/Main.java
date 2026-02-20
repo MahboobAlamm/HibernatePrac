1e58d0f59713d2270c90a2e3a9c9b1986054c152
Mahboob Alam <imahboob4alam@gmail.com>
2026-02-20 11:28:19 +0530

    First program run with ORM mapping with table

--- HibernatePrac/src/main/java/org/example/Main.java
+++ HibernatePrac/src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Student st = new Student();
+        st.setsId(1);
+        st.setsName("Mahboob");
+        st.setAge(24);
+
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        cfg.addAnnotatedClass(org.example.Student.class);
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transation = session.beginTransaction();
+        session.persist(st);
+        transation.commit();
     }
 }
