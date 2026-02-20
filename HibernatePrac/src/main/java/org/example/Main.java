4c4aa2fd3b24a3d1f4902c57a550eaacafe32379
Mahboob Alam <imahboob4alam@gmail.com>
2026-02-20 21:05:37 +0530

    HQL (Hinernate Query Language)

--- HibernatePrac/src/main/java/org/example/Main.java
+++ HibernatePrac/src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.hibernate.QueryException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -15,7 +17,6 @@ public class Main {
         lp1.setlName("Asus");
         lp1.setLmodel("x500");
         lp1.setlYear("2007");
-
 
         Laptop lp2 = new Laptop();
         lp2.setlId(102);
@@ -53,12 +54,15 @@ public class Main {
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
 
+
         Transaction transaction = session.beginTransaction();
         session.persist(lp1);
         session.persist(lp2);
         session.persist(lp3);
         session.persist(st1);
         session.persist(st2);
+
+
 
 //        Student st1 = session.find(Student.class, 5); //to get the values using primary key
 //        System.out.println(st1.getAge());                //If no value found it will return null, and getting that age from null will throw exception, so handle it explicitly
@@ -67,5 +71,25 @@ public class Main {
 //          session.remove(st);                              // to remove the value
 
           transaction.commit();
+
+
+          //HQL (Hibrnate Querey Language)
+        Query<Student> query = session.createQuery("From Student", Student.class);
+        List<Student> students = query.getResultList();
+        for(Student x : students) {
+            System.out.println(x);
+        }
+
+        Query<Integer> query1 = session.createQuery("Select age from Student where age = ?1 ");
+        query1.setParameter(1, 24);
+
+        List<Integer> ages = query1.getResultList();
+        int count = 0;
+        for(Integer x : ages) {
+            count++;
+//            System.out.println(x);
+        }
+        System.out.println("Total no of people with age equal to 24 : " + count);
+
     }
 }
